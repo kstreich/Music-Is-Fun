783659690bad8e08cb2a5f323a76ec816a5948e6
Kimberly Streicher <kimberlystreicher@gmail.com>
2018-11-09 15:56:03 -0700

    songs up

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -7,9 +7,22 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
+  let songs = itunesService.getMusicByArtist()
+  let template = ""
 
-
-
+  results.forEach(result => {
+    template += `
+    <div class= "col card">
+      <img src="${result.albumArt}">
+      <h5>${result.title}</h5>
+      <h5>${result.artist}</h5>
+      <p>${result.collection}</p>
+      <p>${result.price}</p>
+      <audio controls src="${result.preview}"></audio>
+    </div>
+    `
+  })
+  document.getElementById("songs").innerHTML = template
 }
 
 
@@ -30,6 +43,47 @@ class ItunesController {
 
 
 }
-
-
 export default ItunesController
+
+
+//     event.preventDefault();
+//     let form = event.target
+//     let formData = {
+//     title: form.title.value,
+//     albumArt: form.albumArt.value,
+//     artist: form.artist.value,
+//     colletion: form.collection.value,
+//     price: form.price.value,
+//     preview: form.preview.value
+//     }
+//   }
+// }
+
+
+
+  // `<div class="container">
+  //       <div class="row">
+  //           <div class="col-sm-4">
+  //               <div id="songs">
+  //                   <div class="form-group">
+  //                       <label for="title">Song Title</label>
+  //                   </div>
+  //                   <div class="form-group">
+  //                       <label for="albumArt">Img</label>
+  //                   </div>
+  //                   <div class="form-group">
+  //                       <label for="artist">Artist</label>
+  //                   </div>
+  //                   <div class="form-group">
+  //                       <label for="colletion">Collection</label>
+  //                   </div>
+  //                   <div class="form-group">
+  //                       <label for="price">Price</label>
+  //                   </div>
+  //                   <div class="form-group">
+  //                       <label for="preview">Preview</label>
+  //                   </div>
+  //               </div>
+  //           </div>
+  //       </div>
+  //   </div>`
